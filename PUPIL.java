86d02c89bd38624f2d7dfbea03d1284199b5b1ce
Guest User <Guest@stk-cs01-ws11.westlothian.sch.uk>
2015-12-03 03:42:25 -0800

    sprint 1.3

--- PUPIL.java
+++ PUPIL.java
@@ -6,6 +6,27 @@ public class PUPIL
 
     public PUPIL()
     {
-        
+       fName = "";
+       sName = "";
+       TopMark = 0;
     } 
+    
+    public void readPupilDetails(String dataItems)
+    {
+        String[] rowItems = dataItems.split(",");
+        fName = rowItems[0];
+        sName = rowItems[1];
+        TopMark = Integer.parseInt(rowItems[2]);
+    }
+    
+    public String writeDetails()
+    {
+        String pupilData = "";
+        pupilData = pupilData.concat(fName);
+        pupilData = pupilData.concat(",");
+        pupilData = pupilData.concat(sName);
+        pupilData = pupilData.concat(",");
+        pupilData = pupilData.concat(Integer.toString(TopMark));
+        return pupilData;
+    }
 }
