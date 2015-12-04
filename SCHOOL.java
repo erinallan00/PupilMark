f7f4dea8fe48732b8c78446906464b931b32841b
Guest User <Guest@stk-cs01-ws11.westlothian.sch.uk>
2015-12-04 01:45:01 -0800

    sprint2.2

---
+++ SCHOOL.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+import java.io.*;
+public class SCHOOL
+{
+   private PUPIL pupillist[];
+   int noOfPupils;
+   FILEREADCSV scoreFile;
+   public SCHOOL()
+   {
+       scoreFile = new FILEREADCSV();
+   }
+
+   public void processPupils() throws IOException
+
+   {
+       setUpPupilList();
+       countOKTopMark();
+   }
+   
+   public void setUpPupilList() throws IOException
+
+   {
+       System.out.println("School top mark update");
+       System.out.println("** Preparing to read data file.");
+       String[] dataRows = scoreFile.readCSVtable();
+       noOfPupils = dataRows.length - 1;
+       System.out.println("** " + noOfPupils + " rows read.\n\n");
+    }
+
+  public void countOKTopMark()
+  {
+       // placeholder
+  }
+
+
+}
