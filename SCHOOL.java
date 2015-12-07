bba0d06e4a6979d2b3669ce1a8983ae6bdc691ed
Guest User <Guest@stk-cs01-ws11.westlothian.sch.uk>
2015-12-07 02:34:07 -0800

    sprint3.0

--- SCHOOL.java
+++ SCHOOL.java
@@ -25,11 +25,23 @@ public class SCHOOL
        String[] dataRows = scoreFile.readCSVtable();
        noOfPupils = dataRows.length;
        System.out.println("** " + noOfPupils + " rows read.\n\n");
+       pupillist = new PUPIL[noOfPupils];
+       for  (int i = 0; i < noOfPupils; i++) {
+            pupillist[i] = new PUPIL();
+            pupillist[i].readPupilDetails(dataRows[i+1]);
+       }
     }
 
   public void countOKTopMark()
   {
-       // placeholder
+       for (int i = 0; i < noOfPupils; i++)
+       {
+ 		if ((pupillist[i].getTopMark() > 50))
+ 		{
+ 		    pupillist[i].displayDetails();
+  		}
+ 	}
+
   }
 
 
